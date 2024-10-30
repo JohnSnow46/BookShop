aec08e3ce87712e6086f599d82cd239573bed566
frane <frane.kuna@gmail.com>
2024-10-30 18:12:50 +0100

    remove tiny, gitignore update

--- iBoooks.DataAccess/Migrations/20241030153801_initialAzure.Designer.cs
+++ iBoooks.DataAccess/Migrations/20241030153801_initialAzure.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using iBoooks.DataAccess.Data;
 
@@ -11,9 +12,11 @@ using iBoooks.DataAccess.Data;
 namespace iBoooks.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241030153801_initialAzure")]
+    partial class initialAzure
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
